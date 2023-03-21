239c974509fcac300e486683ec9f89c6bf4cf2b7
asinthelol <asinthexd@gmail.com>
2023-03-21 13:23:54 -0400

    Up-to-date with JP ver. 2.70.0
    -Added all items (including Lostbelt 7)
    -Added a dark mode button
    -Smoother transitions
    -Switched to flexbox for most elements
    
    Plan to give every item its own page and separate the CSS and JavaScript files; they're getting too big now...

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // Variables.
 let itemMatch;
 let itemArr = [];
+const itemButtonContainer = document.getElementById("item-button-container");
+const darkButton = document.getElementById("dark-mode-button");
+const lightButton = document.getElementById("light-mode-button");
+const itemButton = document.getElementById("item-button");
+const searchBarContainer = document.getElementById("search-bar-container");
 const searchBar = document.getElementById("search");
 const itemArea = document.getElementById("item-area");
 const items = document.getElementsByClassName("item");
 const itemName = document.getElementsByTagName("h1");
 const fgoHeader = document.getElementById("header");
 const locationBox = document.getElementsByClassName("location-box");
+const itemBox = document.getElementsByClassName("item-box");
 const phoneMediaQuery = window.matchMedia("(max-width: 500px)");
+const body = document.getElementById("body");
+const textEle = document.getElementsByClassName("text-el");
 
 // Selects all divs with class "item", selects the <h1> tags, and adds their values to the itemArr array.
 for(var i = 0; i < itemName.length; i++) {
@@ -23,13 +31,13 @@ searchBar.addEventListener("input", (n) => {
 
         if(itemArr[i].includes(searchValue) && searchValue != "") {
             items[i].style.display = "";
-            itemArea.style.display = "grid";
-            setTimeout( () => {items[i].classList.add("active")}, 100);
+            itemArea.style.display = "flex";
+            items[i].classList.add("active")
         } else {
             items[i].classList.remove("active");
-            setTimeout( () => {items[i].style.display = "none"}, 300);
+            items[i].style.display = "none";
             locationBox[i].classList.remove("active");
-            setTimeout( () => {locationBox[i].style.display = "none"}, 300);
+            locationBox[i].style.display = "none"
         }
     }
 })
@@ -41,13 +49,47 @@ searchBar.addEventListener("focusin", () => {
 })
 
 for(let i = 0; i < items.length; i++) {
-    items[i].addEventListener("click", () => {
+    itemBox[i].addEventListener("click", () => {
         if(locationBox[i].parentElement === items[i] && (!(locationBox[i].classList.contains("active")))) {
-            locationBox[i].style.display = "block";
-            setTimeout( () => {locationBox[i].classList.add("active")}, 100);
+            locationBox[i].style.display = "flex";
+            locationBox[i].style.transition = "height 0.5s, opacity 0.3s ease-in 0.25s, transform 0.5s";
+            locationBox[i].children[1].classList.add("active");
+            setTimeout( () => {locationBox[i].classList.add("active")}, 10);
         } else {
+            locationBox[i].style.transition = "height 0.5s, opacity 0.1s, transform 0.5s";
             locationBox[i].classList.remove("active");
-            setTimeout( () => {locationBox[i].style.display = "none"}, 300);
+            setTimeout( () => {locationBox[i].style.display = "none"}, 500);
         }
     })
 }
+
+itemButton.addEventListener("click",  () => {
+    searchBarContainer.style.display = "block";
+    searchBar.style.display = "block";
+    itemButtonContainer.style.opacity = "0";
+    setTimeout( () => {itemButtonContainer.style.display = "none"}, 150);
+    setTimeout( () => {searchBarContainer.style.opacity = "1"}, 300);
+    setTimeout( () => {searchBar.style.opacity = "1"}, 300);
+})
+
+darkButton.addEventListener("click", () => {
+    body.classList.add("darkMode");
+    darkButton.classList.add("active");
+    lightButton.classList.add("active");
+    itemArea.classList.add("darkMode");
+    for(let i = 0; i < textEle.length; i++) {textEle[i].classList.add("darkMode");}
+    for(let i = 0; i < itemBox.length; i++) {itemBox[i].classList.add("darkMode");}
+    for(let i = 0; i < itemBox.length; i++) {locationBox[i].classList.add("darkMode");}
+    itemButton.classList.add("darkMode");
+})
+
+lightButton.addEventListener("click", () => {
+    body.classList.remove("darkMode");
+    darkButton.classList.remove("active");
+    lightButton.classList.remove("active");
+    itemArea.classList.remove("darkMode");
+    for(let i = 0; i < textEle.length; i++) {textEle[i].classList.remove("darkMode");}
+    for(let i = 0; i < itemBox.length; i++) {itemBox[i].classList.remove("darkMode");}
+    for(let i = 0; i < itemBox.length; i++) {locationBox[i].classList.remove("darkMode");}
+    itemButton.classList.remove("darkMode");
+})
